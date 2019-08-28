957e490418208b163336e47ddfd874c9cbdfbd45
roman <pavlicekr@email.cz>
2019-08-28 18:15:16 +0200

    add build

---
+++ client/build/precache-manifest.c59813cbf3294aaef759951d198675db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c94b088cf89a2815c88557fa00699b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "866636fa80598fed5aac",
+    "url": "/static/css/2.ad78052d.chunk.css"
+  },
+  {
+    "revision": "248ed980dadfe734da74",
+    "url": "/static/css/main.10c28748.chunk.css"
+  },
+  {
+    "revision": "866636fa80598fed5aac",
+    "url": "/static/js/2.a4b1a37f.chunk.js"
+  },
+  {
+    "revision": "248ed980dadfe734da74",
+    "url": "/static/js/main.9811ff8a.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
