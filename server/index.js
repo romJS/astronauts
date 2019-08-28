b1efb4608aac1565e13950a61c8d3cbb37346520
roman <pavlicekr@email.cz>
2019-08-28 19:00:50 +0200

    ch

--- server/index.js
+++ server/index.js
@@ -87,7 +87,7 @@ app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
 
 
 	if(process.env.NODE_ENV === "production") {
-		app.use(express.static('client/build'));
+		app.use(express.static('../client/build'));
 
 		app.get('*', (req, res) => {
 			res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
