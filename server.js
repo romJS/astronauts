606c08a4d1834f0befe499e4cf42196f535daa01
Roman <pavlicekr@email.cz>
2019-08-28 14:06:17 +0200

    ch

--- server.js
+++ server.js
@@ -59,7 +59,11 @@ app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
 
 
 	if(process.env.NODE_ENV === "production") {
-        app.use(express.static(path.resolve(__dirname, 'client/build')));
+        app.use(express.static('client/build'));
+
+        app.get('/astronauts', (req, res) => {
+            res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+        })
 
         app.get('/astronauts', (req, res) => {
             const { error } = validateGet(req.query);
@@ -87,9 +91,9 @@ app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
                     res.json(person);
             });
         });
-        // app.get('/astronauts', (req, res) => {
-        //     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-        // })
+        app.get('/astronauts', (req, res) => {
+            res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+        })
     }
 
 // ---------------------------------------------------------------------------
