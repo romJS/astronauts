88201f8b208cda26fd3c689f096f17f30f30357d
Roman <pavlicekr@email.cz>
2019-08-28 14:09:03 +0200

    ch

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
 	if(process.env.NODE_ENV === "production") {
         app.use(express.static('client/build'));
 
-        app.get('/astronauts', (req, res) => {
+        app.get('*', (req, res) => {
             res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
         })
 
