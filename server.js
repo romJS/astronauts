f985bcaedc66c77f4f5e9f379c1103d249b6809b
Roman <pavlicekr@email.cz>
2019-08-28 14:41:43 +0200

    ch

--- server.js
+++ server.js
@@ -84,6 +84,15 @@ app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
 		});
 	});
 
+
+	if(process.env.NODE_ENV === "production") {
+		app.use(express.static('client/build'));
+
+		app.get('*', (req, res) => {
+			res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+		})
+	}
+
 // ---------------------------------------------------------------------------
 
 // POST requests -------------------------------------------------------------	
